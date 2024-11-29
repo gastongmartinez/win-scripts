6a962521dd3acae8e6fdf774f9ddbe441c6f148f
Gastón Martínez <gastongmartinez@gmail.com>
2024-11-29 14:26:34 -0300

    Instala Chocolatey y WinGet

---
+++ Win10/2_PkgsMgrs.ps1
@@ -0,0 +1,18 @@
+Write-Output "Instalando Chocolatey"
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Output "Instalando WinGet"
+
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
+
+$SN = Read-Host -Prompt "Es necesario reiniciar el equipo, desea hacerlo ahora? (S/N): "
+if ( $SN -eq "S" ) {
+    Restart-Computer
+}
